85be9f61b10270445136961e899fc61a4ab3d7d7
pkondrashkov <pkondrashkov@gmail.com>
2021-07-22 16:43:45 +0300

    +

--- Package.swift
+++ Package.swift
@@ -4,8 +4,10 @@ import PackageDescription
 let package = Package(
     name: "FaceSDK",
     products: [
-        // .library(name: "FaceSDK", targets: ["FaceSDK-Target"])
-        .library(name: "FaceSDK", targets: ["FaceSDK", "RegulaCommon"])
+        .library(name: "FaceSDK", targets: ["FaceSDKTarget"])
+    ],
+    dependencies: [
+        .package(name: "RegulaCommon", url: "https://github.com/pkondrashkov/common", .branch("master")),
     ],
     targets: [
         .binaryTarget(
@@ -13,18 +15,13 @@ let package = Package(
             url: "https://pods.regulaforensics.com/RegulaCommon/face-sdk/FaceSDK.xcframework.zip",
             checksum: "9e302183b37b56f00bca52c843f2dce072d76deae8b90cae776261cf75d7f2fa"
         ),
-        .binaryTarget(
-            name: "RegulaCommon",
-            url: "https://pods.regulaforensics.com/RegulaCommon/regula-common/RegulaCommon.xcframework.zip",
-            checksum: "9ef592c40d960420c0dba3c49e97f2f3071baaff60a1e00b2b9d99f6cf6d656a"
-        ),
-        // .target(
-        //     name: "FaceSDK-Target",
-        //      dependencies: [
-        //             .target(name: "FaceSDK", condition: .when(platforms: .some([.iOS]))),
-        //             .target(name: "RegulaCommon", condition: .when(platforms: .some([.iOS])))
-        //     ],
-        //     path: "FaceSDK-Target"
-        // )
+        .target(
+            name: "FaceSDKTarget",
+             dependencies: [
+                    .target(name: "FaceSDK", condition: .when(platforms: .some([.iOS]))),
+                    .product(name: "RegulaCommon", package: "RegulaCommon")
+            ],
+            path: "Sources"
+        )
     ]
 )
