d6d20d29bb733307eca83711e6b4c7be99ae5b79
pkondrashkov <pkondrashkov@gmail.com>
2021-07-22 11:23:35 +0300

    https

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FaceSDK",
-            url: "http://ftp.regula.local/iOS/RegulaCommon/face-sdk/0.0.1.zip",
+            url: "https://pods.regulaforensics.com/RegulaCommon/face-sdk/0.0.1.zip",
             checksum: "0cf35bc5626b81964269d4e8ffa9954fffdb87900883e686bab73f0dc83fc841"
         ),
         .binaryTarget(
             name: "RegulaCommon",
-            url: "http://ftp.regula.local/iOS/RegulaCommon/regula-common/0.0.1.zip",
+            url: "https://pods.regulaforensics.com/RegulaCommon/regula-common/0.0.1.zip",
             checksum: "2bbadce710ede2a4c5fc58991122b399ec18703aae9b59d00ae6a502aa7952fd"
         ),
         // .target(
