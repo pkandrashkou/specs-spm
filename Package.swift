d25e27ed067e219a8f855c0ec2d1dcd7ad48f078
pkondrashkov <pkondrashkov@gmail.com>
2021-07-22 11:11:57 +0300

    test

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "FaceSDK",
+    products: [
+        .library(name: "FaceSDK", targets: ["FaceSDK", "RegulaCommon"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "FaceSDK",
+            url: "http://ftp.regula.local/iOS/RegulaCommon/face-sdk/0.0.1.zip",
+            checksum: "0cf35bc5626b81964269d4e8ffa9954fffdb87900883e686bab73f0dc83fc841"
+        ),
+        .binaryTarget(
+            name: "RegulaCommon",
+            url: "http://ftp.regula.local/iOS/RegulaCommon/regula-common/0.0.1.zip",
+            checksum: "2bbadce710ede2a4c5fc58991122b399ec18703aae9b59d00ae6a502aa7952fd"
+        ),
+        // .target(
+        //     name: "FaceSDK-Target",
+        //      dependencies: [
+        //             .target(name: "FaceSDK", condition: .when(platforms: .some([.iOS]))),
+        //             .target(name: "RegulaCommon", condition: .when(platforms: .some([.iOS])))
+        //     ],
+        //     path: "FaceSDK-Target"
+        // )
+    ]
+)
